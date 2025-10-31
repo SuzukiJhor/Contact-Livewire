6d067fcecb20f872253860bb9cfb1e24db2e3605
jhordan.suzuki <jhordan.kendy1@gmail.com>
2025-10-31 17:08:37 -0300

    feat: add logic to show description when contact list is empty

--- resources/views/livewire/contacts.blade.php
+++ resources/views/livewire/contacts.blade.php
@@ -1,6 +1,12 @@
 <div class="card p-5">
    <p class="mb-3">Contatos</p>
 
+   @if($contacts->isEmpty())
+       <div class="card bk-dark p-3 mb-1 text-center text-white">
+           Nenhum contato cadastrado.
+       </div>
+   @endif
+
    @foreach ($contacts as $contact)
        <div class="card p-3 mb-1">
          <div class="row">
