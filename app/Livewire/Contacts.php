18425ad837ab46b482420168a3c2144fe5674f0f
jhordan.suzuki <jhordan.kendy1@gmail.com>
2025-11-03 12:31:07 -0300

    feat: add filter field to filter results in live

--- app/Livewire/Contacts.php
+++ app/Livewire/Contacts.php
@@ -10,6 +10,9 @@ use Livewire\WithPagination;
 class Contacts extends Component
 {
     use WithPagination;
+
+    public string $searchTerm = '';
+    private int $contactsPerPage = 5;
   
     #[on('contactAdded')]
     public function updateContactList()
@@ -18,6 +21,14 @@ class Contacts extends Component
 
     public function render()
     {
-        return view('livewire.contacts')->with('contacts', Contact::paginate(5));
+        $contacts = null;
+        if ($this->searchTerm) {
+            $contacts = Contact::where('name', 'like', '%' . $this->searchTerm . '%')
+                ->orWhere('email', 'like', '%' . $this->searchTerm . '%')
+                ->orWhere('phone', 'like', '%' . $this->searchTerm . '%')
+                ->paginate($this->contactsPerPage);
+            return view('livewire.contacts')->with('contacts', $contacts);
+        }
+        return view('livewire.contacts')->with('contacts', Contact::paginate($this->contactsPerPage));
     }
 }
--- resources/views/livewire/contacts.blade.php
+++ resources/views/livewire/contacts.blade.php
@@ -1,29 +1,34 @@
 <div class="card p-5">
-   <p class="mb-3">Contatos</p>
+    <p class="mb-3 fw-semibold">Contatos</p>
 
-   @if($contacts->isEmpty())
-       <div class="card bk-dark p-3 mb-1 text-center text-white">
-           Nenhum contato cadastrado.
-       </div>
-   @endif
+    <div class="d-flex align-items-center gap-2 mb-3">
+        <span class="fw-semibold">Buscar:</span>
+        <input type="text" class="form-control form-control-sm w-auto" style="max-width: 250px"
+            wire:model.live="searchTerm" placeholder="Nome, email ou telefone">
+    </div>
+    @if ($contacts->isEmpty())
+        <div class="card bk-dark p-3 mb-1 text-center text-white">
+            Nenhum contato cadastrado.
+        </div>
+    @endif
 
-   @foreach ($contacts as $contact)
-       <div class="card p-3 mb-1">
-         <div class="row">
-             <div class="col">  Nome: {{ $contact->name }}</div>
-             <div class="col">  Email: {{ $contact->email }}</div>
-             <div class="col">  Telefone: {{ $contact->phone }}</div>
-             <div class="col"> 
-                    <a href="{{route('contacts.delete', ['id' => $contact->id])}}" class=" btn btn-danger">Deletar</a>
+    @foreach ($contacts as $contact)
+        <div class="card p-3 mb-1">
+            <div class="row">
+                <div class="col"> Nome: {{ $contact->name }}</div>
+                <div class="col"> Email: {{ $contact->email }}</div>
+                <div class="col"> Telefone: {{ $contact->phone }}</div>
+                <div class="col">
+                    <a href="{{ route('contacts.delete', ['id' => $contact->id]) }}" class=" btn btn-danger">Deletar</a>
+                </div>
+                <div class="col">
+                    <a href="{{ route('contacts.edit', ['id' => $contact->id]) }}" class=" btn btn-primary">Editar </a>
+                </div>
             </div>
-             <div class="col"> 
-                    <a href="{{route('contacts.edit', ['id' => $contact->id])}}" class=" btn btn-primary">Editar </a>
-            </div>
-         </div>
-       </div>
-       <hr class="m-0 p-0">   
-   @endforeach
-   <div>
-    {{ $contacts->links() }}
-   </div>
+        </div>
+        <hr class="m-0 p-0">
+    @endforeach
+    <div>
+        {{ $contacts->links() }}
+    </div>
 </div>
